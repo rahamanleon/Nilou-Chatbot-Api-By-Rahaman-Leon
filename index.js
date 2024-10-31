94c25b40bc2dd2ad8ee8ffd0bc5d5d8d0f23f878
rahamanleon <92204631+rahamanleon@users.noreply.github.com>
2024-10-31 14:15:31 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+require('./app'); // Import the app from app.js
+
+// Since the server starts in app.js, we do not need to listen again here.
+// This file can remain empty or can be used for future expansions.
